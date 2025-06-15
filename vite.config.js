4788fa6d41c5039e322df55e1426d761fd31eb59
tfgkk <1316187067@qq.com>
2025-06-15 16:03:52 +0800

    Update configuration for development and production environments

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Studyplan/",
+  base: process.env.NODE_ENV === "production" ? "/Studyplan/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
