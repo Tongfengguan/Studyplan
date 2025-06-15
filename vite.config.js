d4bf456e45b46a532cbad273b2b48ab1a0e26d4f
tfgkk <1316187067@qq.com>
2025-06-15 15:57:03 +0800

    Update resource paths and build configuration

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,18 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "favicon.ico") {
+            return "favicon.ico";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
   publicDir: "public",
 });
